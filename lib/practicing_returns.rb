c8cbae592c59d9c569a96d56916940afae37d97a
Caleb Aviles <earthctzn1@gmail.com>
2019-04-20 20:09:26 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -3,11 +3,11 @@ require 'pry'
 def hello(array)
   i = 0
   while i < array.length
-  bining pry
+  binding pry
     yield(array[i])
     i += 1
   end
 end
 
-#bining pry
+#binding pry
 hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
